028fffce003342e35a687c272ede5a737910f594
henriquebcustodio <henriquebcustodio@outlook.com>
2022-01-31 20:38:46 -0300

    Added intro

--- scripts/modules/navToggle.js
+++ scripts/modules/navToggle.js
@@ -2,12 +2,17 @@ const openNav = document.querySelector('.header__open-nav');
 const closeNav = document.querySelector('.header__close-nav');
 const navbar = document.querySelector('.navbar');
 const backdrop = document.querySelector('.backdrop');
+const body = document.body;
+let isOpen = false;
 
 const toggle = () => {
     openNav.classList.toggle('header__open-nav--visible');
     closeNav.classList.toggle('header__close-nav--visible');
     navbar.classList.toggle('navbar--visible');
     backdrop.classList.toggle('backdrop--visible');
+    isOpen = !isOpen;
+
+    isOpen ? body.style.overflowY = 'hidden' : body.style.overflowY = 'auto';
 };
 
 const setNavToggle = () => {
